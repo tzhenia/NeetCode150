0056e7f1c750fc7aba15707b065ae1aab7874cb6
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-10-12 14:47:03 +0300

    Create 6. [Medium] 271. Encode and Decode Strings.java

---
+++ 1. Arrays & Hashing/6. [Medium] 271. Encode and Decode Strings.java
@@ -0,0 +1,49 @@
+// Encoding & Decoding (Optimal)
+// Time complexity: O(m) for each encode() and decode() function calls.
+// Space complexity: O(m+n) for each encode() and decode() function calls.
+
+public class Codec {
+    private static final char DELIMETR = '#';
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : strs) {
+            sb.append(s.length());
+            sb.append(DELIMETR);
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+
+            while (str.charAt(j) != DELIMETR) {
+                j++; // count how many numbers 1 or 2 or 3 ...(Ex: 1 vs 10 vs 101)
+            }
+
+            int length = Integer.parseInt(str.substring(i, j)); // parse number
+
+            i = j + 1; // move left pointer (index of starting word)
+            j = i + length; // index of ending word
+            String word = str.substring(i, j);
+            res.add(word);
+
+            i = j; // move pointer to next word;
+        }
+
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
