7ed210a318fb541f9e75e9e2d542fbff0835f1a0
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-10-12 14:37:46 +0300

    Update and rename 4. [Medium] 49. Group Anagrams to 4. [Medium] 49. Group Anagrams.java

--- 1. Arrays & Hashing/4. [Medium] 49. Group Anagrams.java
+++ 1. Arrays & Hashing/4. [Medium] 49. Group Anagrams.java
@@ -3,6 +3,7 @@
 // Space complexity: 
 //     O(m) extra space. 
 //     O(m*n) space for the output list.
+// Where m is the number of strings and is the length of the longest string.
 
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
