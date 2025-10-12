d1bceef9c51aacb5a3db476b152b5c238b94b39c
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-10-12 12:49:56 +0300

    Create [Easy] 217. Contains Duplicate.java

---
+++ Arrays & Hashing/[Easy] 217. Contains Duplicate.java
@@ -0,0 +1,19 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        var set = new HashSet<Integer>();
+
+        for (var i : nums) {
+            if (set.contains(i)) {
+                return true;
+            }
+
+            set.add(i);
+        }
+
+        return false;
+    }
+}
