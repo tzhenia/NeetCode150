a4814ea8a79ca7e7de8667d377a333c26ec6dfe7
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-10-14 08:29:45 +0300

    Create 11. [Medium] Two Sum II - Input Array Is Sorted.java

---
+++ 2. Two Pointers/11. [Medium] Two Sum II - Input Array Is Sorted.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0;
+        int r = numbers.length - 1;
+        int[] ans = new int[2];
+
+        while (l < r) {
+            int currSum = numbers[l] + numbers[r];
+            if (currSum > target) {
+                r--;
+            } else if (currSum < target) {
+                l++;
+            } else {
+                ans[0] = l + 1; // +1 The index in this task started from 1 (not as usual from 0)
+                ans[1] = r + 1; // +1 The index in this task started from 1 (not as usual from 0)
+                return ans;
+            }
+        }
+
+        return new int[0];
+    }
+}
