267d25ffba8fcf3f028ac179f7da77b05e27d1fd
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-10-15 09:58:23 +0300

    Update 3. [Easy] 1. Two Sum.java

--- 1. Arrays & Hashing/3. [Easy] 1. Two Sum.java
+++ 1. Arrays & Hashing/3. [Easy] 1. Two Sum.java
@@ -5,17 +5,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
 
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
 
             if (map.containsKey(complement)) {
-                return new int[] { i, map.get(complement) };
+                ans[0] = i;
+                ans[1] = map.get(complement);
             }
 
             map.put(nums[i], i);
         }
 
-        return new int[] {};
+        return ans;
     }
 }
