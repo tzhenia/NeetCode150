89eed4300f30f5248dd6728210662b0f918717d2
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-10-15 09:40:04 +0300

    Create 12. [Medium] 15. 3Sum.java

---
+++ 2. Two Pointers/12. [Medium] 15. 3Sum.java
@@ -0,0 +1,47 @@
+// Time complexity: O(n 2)
+// Space complexity:
+//   O(1) or O(n) extra space depending on the sorting algorithm.
+//   O(m) space for the output list.
+
+class Solution {
+    public static final int TARGET = 0; // Task requirement
+
+    // if we sort input nums, idea of solution will be similar with https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > TARGET) { // After sorting, the first element can't be bigger than zero (TARGET).
+                break;
+            }
+
+            if (i > 0 && nums[i - 1] == nums[i]) { // Trick: left and right number can't be the same.
+                continue;
+            }
+
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > TARGET) {
+                    r--;
+                } else if (sum < TARGET) {
+                    l++;
+                } else {
+                    List<Integer> triplet = List.of(nums[i], nums[l], nums[r]);
+                    ans.add(triplet);
+                    l++;
+                    //r--;
+
+                    // Trick: left and right number can't be the same.
+                    while (l < r && nums[l - 1] == nums[l]) {
+                        l++;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
