7acb3cec4152465545791c088257ea5174759fa8
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-10-18 15:37:50 +0300

    Create 14. [Easy] 121. Best Time to Buy and Sell Stock.java

---
+++ 3. Sliding Window/14. [Easy] 121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,17 @@
+// Dynamic Programming (?)
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+
+        int minPrice = prices[0];
+        for (int currPrice : prices) {
+            minPrice = Math.min(minPrice, currPrice);
+            maxProfit = Math.max(maxProfit, currPrice - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
