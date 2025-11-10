290c3632b8947bd469be068f1d5389356f1c2a3b
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-11-10 20:42:16 +0200

    Create 18. [Hard] 76. Minimum Window Substring.java

---
+++ 3. Sliding Window/18. [Hard] 76. Minimum Window Substring.java
@@ -0,0 +1,54 @@
+// Sliding Window + 2 HashMaps + have + need
+// Time complexity: O(n+m)
+// Space complexity: O(m)
+//   Where n is the length of the string s and m is the total number of unique characters in the strings t and s.
+  
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.isEmpty()) {
+            return "";
+        }
+
+        Map<Character, Integer> countT = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            countT.put(c, countT.getOrDefault(c, 0) + 1);
+        }
+
+        int have = 0;
+        int need = countT.size();
+        int[] ans = {-1, -1};
+        int ansLength = Integer.MAX_VALUE;
+
+        int l = 0;
+        int r = 0;
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
+                have++;
+            }
+
+            while (have == need) {
+                if ((r - l + 1) < ansLength) {
+                    ansLength = r - l + 1;
+                    ans[0] = l;
+                    ans[1] = r;
+                }
+
+                char leftChar = s.charAt(l);
+                window.put(leftChar, window.get(leftChar) - 1);
+                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
+                    have--;
+                }
+
+                l++;
+            }
+            
+            r++;
+        }
+        
+        return ansLength == Integer.MAX_VALUE ? "" : s.substring(ans[0], ans[1] + 1);
+    }
+}
