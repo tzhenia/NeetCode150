389fc552f921b1dd6de6e78c1910b462faa7dc24
Yevhenii Taranukha <tzhenia31@gmail.com>
2025-11-15 15:40:17 +0200

    Update 20. [Hard] 239. Sliding Window Maximum.java

--- 3. Sliding Window/20. [Hard] 239. Sliding Window Maximum.java
+++ 3. Sliding Window/20. [Hard] 239. Sliding Window Maximum.java
@@ -10,16 +10,22 @@ class Solution {
         int l = 0;
         int r = 0;
         while (r < nums.length) {
-            // remove 
-            while (!q.isEmpty() && nums[q.getLast()] < nums[r]) {
+            // Remove all smaller elements from queue
+            // We don’t need those elements anymore — the current element is larger, 
+            // which means it will “live” longer in the deque.
+            // This keeps the deque in a strictly decreasing order of values.
+            while (!q.isEmpty() && nums[r] > nums[q.getLast()]) {
                 q.removeLast();
             }
+
             q.addLast(r);
 
+            // Remove the element from the front if it has moved out of the window.
             if (l > q.getFirst()) {
                 q.removeFirst();
             }
 
+            // Once the window becomes full, we save the maximum value.
             if ((r + 1) >= k) {
                 ans[l] = nums[q.getFirst()];
                 l++;
